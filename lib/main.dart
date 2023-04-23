6d6b7d9f28855cb6d38640e07c3b0f458c647897
kim7881 <86556384+kim7881@users.noreply.github.com>
2023-04-23 11:04:28 +0900

    Go 함수

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:go_router_example/screen/1_screen.dart';
 import 'package:go_router_example/screen/home_screen.dart';
 
 void main() {
@@ -9,14 +10,20 @@ void main() {
 }
 
 class _App extends StatelessWidget {
-  const _App({Key? key}) : super(key: key);
+  _App({Key? key}) : super(key: key);
 
-  GoRouter get _router => GoRouter(
-    initialLocation: '/',
+  final GoRouter _router = GoRouter(
+        initialLocation: '/',
         routes: [
           GoRoute(
             path: '/',
             builder: (_, state) => HomeScreen(),
+            routes: [
+              GoRoute(
+                path: 'one',
+                builder: (_, state) => OneScreen(),
+              ),
+            ],
           ),
         ],
       );
